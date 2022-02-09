6094d9a3063aaadc5084d4a451c845e2e2cc01e0
rodolfogoulart <rodolfo.goulart@gmail.com>
2022-02-08 23:48:20 -0300

    git html
    flutter build web --web-renderer canvaskit --dart-define=FLUTTER_WEB_USE_SKIA=true --base-href="/flutter_wordle/"

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "28acbe1dc9fa664bcce0c2ec6e451a3d",
-"/": "28acbe1dc9fa664bcce0c2ec6e451a3d",
-"main.dart.js": "70b666c50aefa6299011b65cfd681075",
+"index.html": "d412874de0f92bf1f124a10c3ded2fda",
+"/": "d412874de0f92bf1f124a10c3ded2fda",
+"main.dart.js": "132fddfffc7813f8bac47af61f1684fe",
 "manifest.json": "bcf90e97cffda02924c7e23664a58f3f",
 "version.json": "2c676a6c5ff0bb42ea451b22c98d11e2"
 };
