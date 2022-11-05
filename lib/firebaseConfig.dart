e4b7fc4dcaf9b1d567e749eb26062e08846f8b4e
rodolfogoulart <rodolfo.goulart@gmail.com>
2022-11-05 14:43:31 -0300

    .

--- lib/firebaseConfig.dart
+++ lib/firebaseConfig.dart
@@ -1,4 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 
 const FirebaseOptions optionsFirebase = FirebaseOptions(
-    apiKey: "", authDomain: "", projectId: "", storageBucket: "", messagingSenderId: "", appId: "", measurementId: "");
+    apiKey: "AIzaSyAJfEmrdVPxdpGs1pqb-vjhFYbBY8oWGdg",
+    authDomain: "desafio-palavra-dia.firebaseapp.com",
+    projectId: "desafio-palavra-dia",
+    storageBucket: "desafio-palavra-dia.appspot.com",
+    messagingSenderId: "684148123972",
+    appId: "1:684148123972:web:e8cc0a61311e05dd344f11",
+    measurementId: "G-XNHHXNS0QF");
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ThemeApp {
-  // final Color backgroundColor = const Color.fromARGB(255, 49, 48, 48);
-  final Color backgroundColor = Colors.blue.shade900;
+  final Color backgroundColor = const Color.fromARGB(255, 49, 48, 48);
+  // final Color backgroundColor = Colors.blue.shade900;
   final Color shadownColor = Colors.grey.shade900;
   final Color shadownColor2 = Colors.grey.shade800;
   final Color primaryTextColor = Colors.white;
   final Color atentionTextColor = Colors.red.shade400;
 
   //
-  // final Color keyboardColor = Colors.blueGrey.shade600;
-  final Color keyboardColor = Colors.orange.shade800;
+  final Color keyboardColor = Colors.blueGrey.shade600;
+  // final Color keyboardColor = Colors.orange.shade800;
   final Color keyboardShadownColor = Colors.blueGrey.shade900;
 
   final Color keyboardErrorColor = Colors.red.shade600;
